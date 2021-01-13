bad9e7cd2c7123c8f4bf6ae3bb2f2cab298b1fe2
Keiko Chuang <keiko@innotech.me>
2021-01-13 10:35:47 +0800

    20210113-update-deploy-scripts

--- docker-push-dockerhub.sh
+++ docker-push-dockerhub.sh
@@ -3,4 +3,4 @@
 docker ps
 docker build --file Dockerfile.dev -t keiko15678/ire:app .
 docker image ls
-docker run -it -p 3000:3000 keiko15678/ire:app
+docker push keiko15678/ire:app
--- heroku-deploy.sh
+++ heroku-deploy.sh
@@ -4,6 +4,7 @@
 docker ps
 heroku container:login
 docker build -t app .
+docker image ls
 # docker run -it -p 3000:3000 app
 heroku container:push web
 heroku container:release web
